f09387987fbdd7e10f8386f3e05c0a38c68e1c06
Xavier Munroe <munroexavier@gmail.com>
2022-09-06 23:09:22 -0400

    cypress scaffold

--- client/utils/getTestIds.ts
+++ client/utils/getTestIds.ts
@@ -4,7 +4,7 @@ export default function getTestIDs() {
   if (process.env.NODE_ENV !== 'test') return {};
 
   let ids = new Map();
-  let proxy = new Proxy({} as { [key: string]: any }, {
+  let proxy = new Proxy({} as { [key: string]: string }, {
     get: function (obj, prop) {
       if (ids.has(prop) === false) {
         ids.set(prop, uuid());
